96d977aee2939a3e2e3f50df8a49dac5e9919719
Michael <michael.alexander0008@gmail.com>
2024-06-24 12:02:21 +0100

    Complete Exercise 7.7

--- part-7/country-hook/src/App.jsx
+++ part-7/country-hook/src/App.jsx
@@ -18,7 +18,22 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then(response => setCountry({
+        data: {
+          name: response.data.name.common,
+          capital: response.data.capital[0],
+          population: response.data.population,
+          flag: response.data.flags.png
+        },
+        found: true
+      }))
+      .catch(error => {
+        name === '' ? setCountry(null) : setCountry({found: false})
+      })
+  }, [name])
 
   return country
 }
