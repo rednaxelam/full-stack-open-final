df74718d7ae154e09ff84bf20389d50578c0f077
Michael <michael.alexander0008@gmail.com>
2024-06-30 19:23:45 +0100

    Complete Exercise 7.17

--- part-7/blog-list/frontend/src/components/Header.jsx
+++ part-7/blog-list/frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux"
 import { logOut as logOutAction } from "../reducers/userReducer"
 import { createNotification } from "../reducers/notificationReducer"
+import { Link } from "react-router-dom"
 
 const Header = ({ user }) => {
   if (!user)
@@ -14,12 +15,18 @@ const Header = ({ user }) => {
     })
   }
 
+  const padding = {
+    padding: 5
+  }
+
   return (
     <>
+      <nav>
+        <Link style={padding} to='/'>blogs</Link>
+        <Link style={padding} to='/users'>users</Link>
+        <span style={padding}>{user.name} logged in <button onClick={() => logOut()}>Log Out</button></span>
+      </nav>
       <h2>blogs</h2>
-      <p>
-        {user.name} logged in <button onClick={() => logOut()}>Log Out</button>
-      </p>
     </>
   )
 }
