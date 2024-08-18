e23dcb097db6d7131eea3104a348830668b811a5
Michael <michael.alexander0008@gmail.com>
2024-08-18 18:52:44 +0100

    Complete Exercise 12.10

--- part-12/todo-app/todo-backend/routes/index.js
+++ part-12/todo-app/todo-backend/routes/index.js
@@ -6,6 +6,9 @@ const configs = require('../util/config')
 
 let visits = 0
 
+initialiseRedis = async () => await redis.setAsync("added_todos", 0)
+initialiseRedis()
+
 /* GET index data. */
 router.get('/', async (req, res) => {
   visits++
@@ -15,5 +18,10 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+router.get('/statistics', async (req, res) => {
+  const addedTodos = await redis.getAsync("added_todos")
+  res.send({ addedTodos })
+})
 
 module.exports = router;
--- part-12/todo-app/todo-backend/routes/todos.js
+++ part-12/todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo')
 const router = express.Router();
+const redis = require('../redis/index');
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,10 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false
   })
+
+  const totalAddedNotes = await redis.getAsync("added_todos")
+  await redis.setAsync("added_todos", Number(totalAddedNotes) + 1)
+
   res.send(todo);
 });
 
